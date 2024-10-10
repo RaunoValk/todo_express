b5735e2b795216d8d0287945ab33a3e41d03434a
RV <rauno.valk@voco.ee>
2024-10-10 13:56:45 +0300

    Read data from file

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require ('express')
 const app = express()
+const fs = require('node:fs');
 
 const path= require("path")
 app.set('view engine', "ejs")
 app.set('views' , path.join(__dirname, 'views'))
 
-app.get('/', (req, res) =>  { 
-    res.render('index')
+app.get('/', (req, res) =>  {
+    
+fs.readFile('./tasks', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+   const tasks =data.split("\n") 
+    res.render('index',{tasks: tasks})
+});
 })
 
 
