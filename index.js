b2e1b1034049e6d48f4bbd39f0f43c59df4376c7
RV <rauno.valk@voco.ee>
2024-10-10 15:38:47 +0300

    Write data to file

--- index.js
+++ index.js
@@ -1,21 +1,55 @@
 const express = require ('express')
 const app = express()
 const fs = require('node:fs');
-
 const path= require("path")
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', "ejs")
 app.set('views' , path.join(__dirname, 'views'))
 
-app.get('/', (req, res) =>  {
+
+const readFile = (filename) =>{
+    return new Promise((resolve, reject) =>{
+        fs.readFile(filename, 'utf8', (err, data) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+             const tasks =data.split("\n") 
+              resolve(tasks)
+              });
+    })
     
-fs.readFile('./tasks', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-   const tasks =data.split("\n") 
-    res.render('index',{tasks: tasks})
-});
+    
+    }
+
+
+app.get('/', (req, res) =>  {
+    readFile('./tasks')
+    .then(tasks =>{ 
+        res.render('index',{tasks: tasks})
+    
+    
+
+    
+    })
+})
+app.post('/', (req, res) =>  {
+    console.log("Form sent data")
+    let task = req.body.task
+    
+    readFile('./tasks')
+    .then(tasks =>{ 
+        tasks.push(task)
+        const data= (tasks.join('\n'))
+        fs.writeFile('./tasks', data, err => {
+            if (err) {
+              console.error(err);
+            } else {
+                res.redirect('/')           
+             }
+          })
+    
+    }) 
 })
 
 
