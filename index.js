c3993a2b9eaf59ea1c34edf3c91c9af1ffb6c8ba
RV <rauno.valk@voco.ee>
2024-10-18 11:24:35 +0300

    Delete single task data from file

--- index.js
+++ index.js
@@ -67,6 +67,26 @@ app.post('/', (req, res) =>  {
     
     }) 
 })
+app.get ('/delete-task/:taskId', (req,res)=>{
+    let deletedTaskId = parseInt(req.params.taskId)
+    readFile('./tasks.json')
+    .then(tasks =>{
+        tasks.forEach((task, index) => {
+            if(task.id === deletedTaskId) {
+                tasks.splice(index, 1)
+            }    
+        })
+        data = JSON.stringify(tasks, null, 2)
+        fs.writeFile('./tasks.json', data, 'utf-8', err => {
+            if (err) {
+              console.error(err);
+              return;         
+             } else  {
+             res.redirect('/')
+            }
+        })
+    })
+})
 
 
 app.listen(3001, () => { 
