83b7efbee1080c091b90dd1f365ad1a36261d353
RV <rauno.valk@voco.ee>
2024-10-18 12:12:17 +0300

    Clear poolik

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require ('express')
 const app = express()
 const fs = require('node:fs');
+const { stringify } = require('node:querystring');
 const path= require("path")
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', "ejs")
@@ -16,12 +17,20 @@ const readFile = (filename) =>{
             }
              const tasks = JSON.parse(data) 
               resolve(tasks)
-              });
+        });
     })
-    
-    
-    }
-
+}
+const writeFile = (filename, data) =>{
+    return new Promise((resolve, reject) =>{
+        fs.writeFile(filename, data, 'utf8', err => {
+            if (err) {
+              console.error(err);
+              return;
+            } 
+              resolve(true)
+        });
+    })
+} 
 
 app.get('/', (req, res) =>  {
     readFile('./tasks.json')
@@ -50,23 +59,12 @@ app.post('/', (req, res) =>  {
             "id" : index,
             "task" : req.body.task
         } 
-        console.log(newTask)
         tasks.push(newTask)
-        console.log(tasks)
         data = JSON.stringify(tasks, null, 2)
-        console.log(data)
-        fs.writeFile('./tasks.json', data, 'utf-8', err => {
-            if (err) {
-              console.error(err);
-              return;
-            } else {
-                console.log('saved')           
-             }
-             res.redirect('/')
-          })
-    
-    }) 
-})
+        writeFile('./tasks.json', data)
+              res.redirect('/')
+             })
+         })
 app.get ('/delete-task/:taskId', (req,res)=>{
     let deletedTaskId = parseInt(req.params.taskId)
     readFile('./tasks.json')
@@ -77,16 +75,17 @@ app.get ('/delete-task/:taskId', (req,res)=>{
             }    
         })
         data = JSON.stringify(tasks, null, 2)
-        fs.writeFile('./tasks.json', data, 'utf-8', err => {
-            if (err) {
-              console.error(err);
-              return;         
-             } else  {
+       writeFile('./tasks.json', data)
              res.redirect('/')
-            }
+            })
         })
-    })
+
+app.get ('/delete-task/clear', (req,res)=>{
+            const data=(stringify[])
+               writeFile('./tasks.json', data)
+                     res.redirect('/')
 })
+                
 
 
 app.listen(3001, () => { 
